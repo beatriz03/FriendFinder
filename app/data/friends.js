6eef7b9d711d089be33be80be439f101f7011cfb
Beatriz <renderosb@gmail.com>
2019-06-08 18:28:42 -0400

    initial push

---
+++ app/data/friends.js
@@ -0,0 +1,61 @@
+var friendsArray = [
+{
+  'name':'Ahmed',
+  'photo':'#',
+  'scores':[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]}, {
+    'name':'Mary',
+    'photo':'#',
+    'scores':[
+      4,
+      5,
+      3,
+      3,
+      4,
+      5,
+      1,
+      4,
+      3,
+      5
+    ]}, {
+      'name':'Samuel',
+      'photo':'#',
+      'scores':[
+        1,
+        3,
+        5,
+        2,
+        4,
+        2,
+        3,
+        5,
+        1,
+        1
+      ]},{
+    'name':'Johnny',
+    'photo':'#',
+    'scores':[
+      2,
+      1,
+      4,
+      5,
+      5,
+      2,
+      3,
+      1,
+      3,
+      1
+    ]}
+];
+
+module.exports = friendsArray;
